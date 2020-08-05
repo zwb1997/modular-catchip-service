27153a3246bd38797b8ab2a67744eb9ff6a3b2db
= <=>
2020-08-05 22:11:10 +0800

    prepare for ip pools

---
+++ model-module/src/main/java/com/zzz/basemodels/enummodel/HttpResponseCodes.java
@@ -0,0 +1,19 @@
+package com.zzz.basemodels.enummodel;
+
+public enum HttpResponseCodes {
+
+    SUCCESS("200");
+
+    private HttpResponseCodes(String code){
+        this.code = code;
+    }
+    private String code;
+
+    public String StringtCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
