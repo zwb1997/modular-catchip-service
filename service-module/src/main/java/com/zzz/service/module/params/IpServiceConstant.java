63bb687a7e6e2bd7e4624f2c2dd6bdc7a03b71e3
zwb1997 <IIIIINNNCCC@outlook.com>
2020-08-15 00:31:24 +0800

    增加ip常量

---
+++ service-module/src/main/java/com/zzz/service/module/params/IpServiceConstant.java
@@ -0,0 +1,8 @@
+package com.zzz.service.module.params;
+
+public class IpServiceConstant {
+    public static final String XIAO_HUAN_IP = "//ip.ihuan.me/tqdl.html";
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36";
+    public static String HAS_PAGE_REGIX = "tbody > tr";
+    public static String PAGE_NUM_COMMON = "a[href^=?page]";
+}
